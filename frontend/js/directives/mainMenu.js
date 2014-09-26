3a7a9ec99d42ec16eea667bce8159860cf9ef70f
Braden Groom <braden.groom@gmail.com>
2014-09-26 16:20:45 -0500

    adding menu directive

---
+++ frontend/js/directives/mainMenu.js
@@ -0,0 +1,7 @@
+angular.module('openDisclosure')
+    .directive('mainMenu', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/mainMenu.html',
+        }
+    });
