1103ae544e82e2681bc2c80c2789ab6bc932aa1d
Braden Groom <braden.groom@gmail.com>
2014-09-27 14:30:38 -0500

    added Grunfile.js.  Added tasks for building production js files and cleaning environment

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: [
+            "build/",
+            "annotated/"
+        ],
+
+        ngAnnotate: {
+            acmApp: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            "js/**/*.js"
+                        ],
+                        rename: function (dest, src) {
+                            return 'annotated/' + src;
+                        }
+                    },
+                ],
+            },
+        },
+
+        concat: {
+            options: {
+                mangle: false
+            },
+            dist: {
+                src: [
+                    "libs/jquery/jquery.min.js",
+                    "libs/bootstrap/dist/js/bootstrap.min.js",
+                    "libs/angular/angular.min.js",
+                    "libs/angular-route/angular-route.min.js",
+                    "annotated/js/script.js",
+                    "annotated/js/controllers/*.js",
+                    "annotated/js/directives/*.js",
+                    "annotated/js/factories/*.js",
+                    "annotated/js/router.js"
+                ],
+                dest: 'build/js/production.js',
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'build/js/production.js',
+                dest: 'build/js/production.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-ng-annotate');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
+}
