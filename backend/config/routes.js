65c28cb75f4286dd5818e7364427e2d29e79386c
Cooper Filby <cfilby@cis.uab.edu>
2014-09-26 22:18:42 -0500

    Fleshed out REST API v0.1

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -45,9 +45,59 @@ router.get('/:year/organizations', dummyResponse);
  * @apiParam {Date} year
  * @apiParam {String} ids single or comma separated list of ids.
  *
- * @apiDescription Get information on a specific or comma separated list of organization ids..
+ * @apiDescription Get information on a specific or comma separated list of organization ids.
  */
 router.get('/:year/organizations/:ids', dummyResponse);
+
+/**
+ * @api {get} /:year/organizations/:ids/transactions Get Specific Organizations Transactions
+ * @apiName GetSpecificOrganizationsTransactions
+ * @apiGroup Organizations
+ *
+ * @apiParam {Date} year
+ * @apiParam {String} ids single or comma separated list of ids.
+ *
+ * @apiDescription Get a JSON Object containing several lists of organization transactions (mapped by organization id).
+ */
+router.get('/:year/organizations/:ids/transactions', dummyResponse);
+
+/**
+ * @api {get} /:year/organizations/:ids/transactions/:type Get Specific Organizations Transactions By Type
+ * @apiName GetSpecificOrganizationsTransactionsByType
+ * @apiGroup Organizations
+ *
+ * @apiParam {Date} year
+ * @apiParam {String} ids single or comma separated list of ids.
+ * @apiParam {String} type One of four possible transaction types (cash, inkind, receipt, expenditure);
+ *
+ * @apiDescription Get a JSON Object containing several lists of organization transactions (mapped by organization id).
+ */
+// TODO: Decide if this and transactions/ should be :type or explicit (type is more convenient, but less pretty for transactions/
+router.get('/:year/organizations/:ids/transactions/:type', dummyResponse);
+
+/**
+ * @api {get} /:year/organizations/:ids/payees Get Specific Organizations Payees
+ * @apiName GetSpecificOrganizationsPayees
+ * @apiGroup Organizations
+ *
+ * @apiParam {Date} year
+ * @apiParam {String} ids single or comma separated list of ids.
+ *
+ * @apiDescription Get a JSON Object containing several lists of organization payees (mapped by organization id).
+ */
+router.get('/:year/organizations/:ids/payees', dummyResponse);
+
+/**
+ * @api {get} /:year/organizations/:ids/contributors Get Specific Organizations Contributors
+ * @apiName GetSpecificOrganizationsContributors
+ * @apiGroup Organizations
+ *
+ * @apiParam {Date} year
+ * @apiParam {String} ids single or comma separated list of ids.
+ *
+ * @apiDescription Get a JSON Object containing several lists of organization contributors (mapped by organization id).
+ */
+router.get('/:year/organizations/:ids/contributors', dummyResponse);
 
 /**
  * @api {get} /:year/organizations/featured Get Featured Organizations
@@ -104,5 +154,85 @@ router.get('/:year/organizations/committees', dummyResponse);
  */
 router.get('/:year/organizations/committees/featured', dummyResponse);
 
+/**
+ * @api {get} /:year/transactions Get Transactions
+ * @apiName GetTransactions
+ * @apiGroup Transactions
+ *
+ * @apiParam {Date} year
+ *
+ * @apiDescription Get information on all available transactions.
+ */
+router.get('/:year/transactions', dummyResponse);
+
+/**
+ * @api {get} /:year/transactions/type/:type Get Contribution Transactions By Type
+ * @apiName GetContributionTransactionsByType
+ * @apiGroup Transactions
+ *
+ * @apiParam {Date} year
+ * @apiParam {String} type One of four possible transaction types (cash, inkind, receipt, expenditure);
+ *
+ * @apiDescription Get information on all available cash contribution transactions.
+ */
+router.get('/:year/transactions/type/:type', dummyResponse);
+
+/**
+ * @api {get} /:year/transactions/:ids Get Specific Transactions
+ * @apiName GetSpecificTransactions
+ * @apiGroup Transactions
+ *
+ * @apiParam {Date} year
+ * @apiParam {String} ids single or comma separated list of ids.
+ *
+ * @apiDescription Get information on a list of specific transactions.
+ */
+router.get('/:year/transactions/:ids', dummyResponse);
+
+/**
+ * @api {get} /:year/payees Get Payees
+ * @apiName GetPayees
+ * @apiGroup Payees
+ *
+ * @apiParam {Date} year
+ *
+ * @apiDescription Get information on all available payees.
+ */
+router.get('/:year/payees', dummyResponse);
+
+/**
+ * @api {get} /:year/payees/:ids Get Specific Payees
+ * @apiName GetPayees
+ * @apiGroup Payees
+ *
+ * @apiParam {Date} year
+ * @apiParam {String} ids single or comma separated list of ids.
+ *
+ * @apiDescription Get information on specific payees.
+ */
+router.get('/:year/payees/:ids', dummyResponse);
+
+/**
+ * @api {get} /:year/contributors Get Contributors
+ * @apiName GetContributors
+ * @apiGroup Contributors
+ *
+ * @apiParam {Date} year
+ *
+ * @apiDescription Get information on all available payees.
+ */
+router.get('/:year/contributors', dummyResponse);
+
+/**
+ * @api {get} /:year/contributors/:ids Get Specific Contributors
+ * @apiName GetPayees
+ * @apiGroup Contributors
+ *
+ * @apiParam {Date} year
+ * @apiParam {String} ids single or comma separated list of ids.
+ *
+ * @apiDescription Get information on specific contributors.
+ */
+router.get('/:year/contributors/:ids', dummyResponse);
 
 module.exports = router;
