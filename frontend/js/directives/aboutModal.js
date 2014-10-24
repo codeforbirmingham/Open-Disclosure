cd0a5c815a732dbe214ef024ee12d5eb8b07bf49
thestuckster <sstucky3@gmail.com>
2014-10-24 10:46:30 -0500

    added a simple modal html file and js directive

---
+++ frontend/js/directives/aboutModal.js
@@ -0,0 +1,7 @@
+angular.module('openDisclosure')
+    .directive('aboutModal', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/aboutModal.html',
+        }
+    });
