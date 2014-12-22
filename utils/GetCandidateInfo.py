6af92986eb70cf3974ced65de9948aa3bee70c3e
mleeds95 <mwl458@gmail.com>
2014-12-22 17:35:33 -0600

    Improve GetCandidateInfo.py to use all the ocd IDs we have. I'll upload the data later.

--- utils/GetCandidateInfo.py
+++ utils/GetCandidateInfo.py
@@ -3,7 +3,7 @@
 ###################################################################
 #
 # File: GetCandidateInfo.py
-# Last Edit: 2014-12-19
+# Last Edit: 2014-12-22
 # Author: Matthew Leeds <mwl458@gmail.com>
 # Purpose: This script reads the PartyCollection.json file and 
 # makes requests to Google's Civic Information API for more 
@@ -12,89 +12,195 @@
 # your Google Developers Console and put it in the constant below.
 # For Google's documentation, see:
 # https://developers.google.com/civic-information/docs/v2
-# This also makes use of Open Civic Data IDs:
+# This makes use of Open Civic Data IDs:
 # http://opencivicdata.readthedocs.org/en/latest/ocdids.html
 #
 ###################################################################
 
+# system libraries
+import os
+import traceback
 import json
+import csv
 from urllib.request import urlopen
 from urllib.parse import quote_plus
+from urllib.error import HTTPError
 from time import sleep
+from operator import itemgetter
 
+# 3rd party libraries
+from fuzzywuzzy import fuzz
+from nameparser import HumanName
+from numpy import mean
+
+# constants
+OCDIDS = os.listdir('../data/ocdIDs/')
 PARTYFILE = '2014_PartyCollection.json'
+CACHEFILE = '.API_Responses_cache.json'
 API_KEY = 'YOUR_API_KEY'
 BASE_URL = 'https://www.googleapis.com/civicinfo/v2/representatives/'
+REQUEST_LIMIT = 25000
 
 def main():
     global allParties
     allParties = [] # all PACs and Candidates
-    # pull the data into memory
+    # pull the party data into memory
     with open('../data/import/' + PARTYFILE) as datafile:
         allParties = json.load(datafile)
-    makeAPIrequests()
-    # write the data back out to the same file
+    global allOCDIDs
+    allOCDIDs = []
+    # pull the OCD IDs into memory
+    for filename in OCDIDS:
+        with open('../data/ocdIDs/' + filename) as datafile:
+            allOCDIDs += csv.reader(datafile)
+    global allResponses
+    allResponses = {} # to save responses we get from Google
+    # check if there's any cached data from a previous run
+    try:
+        with open(CACHEFILE) as datafile:
+            allResponses = json.load(datafile)
+    except IOError:
+        pass
+    numFailures = 0
+    numRequests = 0
+    # make an API request for each OCD ID
+    for ocdRecord in allOCDIDs:
+        if numRequests == REQUEST_LIMIT:
+            print('>> Error: Usage quota reached (' + str(REQUEST_LIMIT) + ').')
+            break
+        print('>> Requesting data for ' + ocdRecord[1])
+        # catch any errors to ensure the data gets written to disk
+        try:
+            if not makeAPIRequest(ocdRecord[0]):
+                numFailures += 1
+            numRequests += 1
+        except Exception as e:
+            print('>> Caught Error: ' + str(e))
+            traceback.print_exc()
+            break
+    print('>> ' + str(numRequests) + ' requests made.')
+    print('>> ' + str(numFailures) + ' requests failed.') 
+    print('>> Writing party data to ' + PARTYFILE + '.')
     with open('../data/import/' + PARTYFILE, 'w') as datafile:
         json.dump(allParties, datafile, sort_keys=True, 
                   indent=4, separators=(',', ': '))
+    print('>> Writing a copy of API responses to ' + CACHEFILE + '.')
+    with open(CACHEFILE, 'w') as datafile:
+        json.dump(allResponses, datafile) # just in case we need them
 
-def makeAPIrequests():
-    # for AL Congresspeople, make API requests
+# make a representativeInfoByDivision query to Google
+def makeAPIRequest(ocdID):
+    # don't make the request again if we have it cached
+    if ocdID in allResponses:
+        reply = allResponses[ocdID]
+    else:
+        url = BASE_URL + quote_plus(ocdID) + '?key=' + API_KEY 
+        response = urlopen(url)
+        sleep(0.1) # go easy on their servers
+        rawReply = response.read()
+        reply = json.loads(rawReply.decode('utf-8'))
+        # save the response in case we need it later
+        allResponses[ocdID] = reply
+    # process the reply data
+    processSuccess = processReply(reply, ocdID)
+    return processSuccess
+
+# process the reply we received, looking for officials in our database
+def processReply(reply, ocdID):
+    try:
+        offices = reply['offices']
+    except KeyError:
+       return False 
+    divisionName = reply['divisions'][ocdID]['name']
+    for office in offices:
+        officeName = office['name']
+        for officialIndex in office['officialIndices']: 
+            official = reply['officials'][officialIndex]
+            # normalize the data, but don't fail when the data isn't there
+            originalName = HumanName(official['name'])
+            normalName = str(originalName.last) + ', ' + str(originalName.first)
+            official['name'] = normalName # so we scrape the formatted version
+            try:
+                originalParty = official['party']
+                normalParty = ('Democrat' if originalParty == 'Democratic' else originalParty)
+                official['party'] = normalParty # so we scrape the formatted version
+            except KeyError:
+                normalParty = ''
+            try:
+                officialPhone = official['phones'][0]
+            except KeyError:
+                officialPhone = ''
+            try:
+                officialEmail = official['emails'][0]
+            except KeyError:
+                officialEmail = ''
+            # look for a matching person with an orgID
+            possibleMatches = {}
+            for party in allParties:
+                if party['type'] == 'Candidate':
+                    # how likely is it the returned person is the same as in the party data?
+                    matchProbability = 0.0 # 100 = very likely match
+                    # try to match on name, party, office, phone, email, and district
+                    # I wonder if we should weight these by different amounts using coefficients?
+                    matchingRatios = []
+                    matchingRatios.append(fuzz.ratio(party['name'], normalName))
+                    if 'party' in party:
+                        matchingRatios.append(fuzz.ratio(party['party'], normalParty))
+                    if 'office' in party:
+                        matchingRatios.append(fuzz.ratio(party['office'], officeName))
+                    if 'phone' in party:
+                        matchingRatios.append(fuzz.ratio(party['phone'], officialPhone))
+                    if 'email' in party:
+                        matchingRatios.append(fuzz.ratio(party['email'], officialEmail))
+                    if 'district' in party:
+                        matchingRatios.append(fuzz.ratio(party['district'], divisionName))
+                    # take a simple average
+                    matchProbability = mean(matchingRatios)
+                    if matchProbability > 85: # arbitrary minimum threshold
+                        possibleMatches[party['_id']] = matchProbability
+            if len(possibleMatches) == 0:
+                continue # to next official
+            else:
+                # find the orgID with the highest matchProbability
+                bestOrgID = max(possibleMatches.items(), key=itemgetter(1))[0]
+                # add useful info about that official to our database
+                scrapeData(official, bestOrgID)
+    return True
+
+# scrape whatever useful data we can from the reply
+def scrapeData(official, candidateOrgID):
     for party in allParties:
-        if party['type'] == 'Candidate': 
-            if 'district' in party and 'District' in party['district']:
-                ocdID = 'ocd-division/country:us/state:al/'
-                if 'House' in party['district']:
-                    ocdID += 'sldl:' + party['district'][15:]
-                elif 'Senate' in party['district']:
-                    ocdID += 'sldu:' + party['district'][16:]
-                else:
-                    print('>> Error: can\'t understand district ' + party['district'])
-                    continue
-                print('>> Requesting info for ' + party['name'])
-                url = BASE_URL + quote_plus(ocdID) + '?key=' + API_KEY 
-                # we're ready to make the request   
-                response = urlopen(url)
-                if response.getcode() == 200:
-                    rawReply = response.read()
-                else:
-                    print('>> Error: Bad reply from Google. Status: ' + response.getcode())
-                    break
-                reply = json.loads(rawReply.decode('utf-8'))
-                # go easy on their servers
-                sleep(0.5)
-                # process data 
-                try:
-                    candidateInfo = reply['officials'][0]
-                except KeyError:
-                    print('>> Error: No officials found for ' + party['name'])
-                    print(reply)
-                    break
-                # don't fail on fields that aren't there
-                try:
-                    party['phone'] = candidateInfo['phones'][0]
-                except KeyError:    
-                    pass
-                try:
-                    party['url'] = candidateInfo['urls'][0]
-                except KeyError:    
-                    pass
-                try:
-                    party['photoURL'] = candidateInfo['photoUrl']
-                except KeyError:    
-                    pass
-                try:
-                    party['email'] = candidateInfo['emails'][0]
-                except KeyError:    
-                    pass
-                try:
-                    for channel in candidateInfo['channels']:
-                        if channel['type'] == 'Facebook':
-                            party['facebookID'] = channel['id']
-                        elif channel['type'] == 'Twitter':
-                            party['twitterID'] = channel['id']
-                except KeyError:    
-                    pass
+        if party['_id'] == candidateOrgID:
+            try:
+                party['party'] = official['party']
+            except KeyError:
+                pass
+            try:
+                party['phone'] = official['phones'][0]
+            except KeyError:
+                pass
+            try:
+                party['email'] = official['emails'][0]
+            except KeyError:
+                pass
+            try:
+                party['url'] = official['urls'][0]
+            except KeyError:
+                pass
+            try:
+                party['photoURL'] = official['photoUrl']
+            except KeyError:
+                pass
+            try:
+                for channel in official['channels']:
+                    if channel['type'] == 'Facebook':
+                        party['facebookID'] = channel['id']
+                    elif channel['type'] == 'Twitter':
+                        party['twitterID'] = channel['id']
+            except KeyError:
+                pass
+            break
+    return
 
 if __name__=='__main__':
     main()
