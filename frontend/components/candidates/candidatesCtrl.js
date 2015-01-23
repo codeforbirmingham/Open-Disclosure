c2179849d728b2321af69a328c5ede56fc4722ac
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-01-22 19:24:00 -0600

    Compute grossGain and pctSpent in Api.

--- frontend/components/candidates/candidatesCtrl.js
+++ frontend/components/candidates/candidatesCtrl.js
@@ -2,15 +2,6 @@ angular.module('openDisclosure')
     .controller('candidatesCtrl', ['$scope', 'Api', function ($scope, Api) {
         $scope.candidates = Api.getCandidates(2014);
         $scope.lastUpdate = "12/20/2014";
-        $scope.grossGain = function(candidates) {
-            cash  = this.cashContributions;
-            inKind = this.inKindContributions;
-            return cash + inKind;
-        };
-        $scope.pctSpent = function () {
-            return candidate.expenditures / this.grossGain * 100;
-        };
-        
         $scope.searchParams = ["lastname","firstname", "totalSpent", "total spent (%)", "campaign"];
         $scope.isCollapsed= true;
     
