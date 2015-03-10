d271f73f138a63ab62ee20992dac476b947c8f38
Matthew Leeds <mwl458@gmail.com>
2015-03-10 01:11:37 -0500

    Combine FindUniqueOrganizations.py and GetCandidateInfo.py into GenerateParties.py. Add CSV functionality to GenerateDistricts.py

---
+++ utils/GenerateDistricts.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+###################################################################
+#
+# File: GenerateDistricts.py
+# Last Edit: 2015-03-10
+# Author: Matthew Leeds <mwl458@gmail.com>
+# Purpose: This script combines the files in data/ocdIDs so we
+# district information in either JSON or CSV format.
+#
+###################################################################
+
+import os
+import json
+import csv
+
+OCDID_FILES = os.listdir('../data/ocdIDs/')
+OUTFILE = '2014_Districts' # file extension will be added
+HEADERS = ['ocdID', 'name']
+OUTPUT_JSON = False # otherwise CSV
+PRETTY_PRINT = True # controls whitespace in JSON output
+OUTFILENAME = OUTFILE + ('.json' if OUTPUT_JSON else '.csv')
+
+def main():
+    allOCDIDs = []
+    for filename in OCDID_FILES:
+        with open('../data/ocdIDs/' + filename) as datafile:
+            allOCDIDs += csv.reader(datafile)
+    districts = []
+    for ocdRecord in allOCDIDs:
+        thisDistrict = {}
+        thisDistrict['ocdID'] = ocdRecord[0]
+        thisDistrict['name'] = ocdRecord[1]
+        districts.append(thisDistrict)
+    if OUTPUT_JSON:
+        with open('../data/' + OUTFILENAME, 'w') as datafile:
+            if PRETTY_PRINT:
+                json.dump(districts, datafile, sort_keys=True, 
+                          indent=4, separators=(',', ': '))
+            else:
+                json.dump(districts, datafile)
+    else: # output CSV
+        with open('../data/' + OUTFILENAME, 'w') as datafile:
+            writer = csv.DictWriter(datafile, quoting=csv.QUOTE_ALL, fieldnames=HEADERS)
+            writer.writeheader()
+            writer.writerows(districts)
+
+if __name__=='__main__':
+    main()
