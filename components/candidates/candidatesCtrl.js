1169d15f3dbbfd21d971f05203bd335d4d498a31
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-06-03 08:15:24 -0500

    Foundationify UI.

--- components/candidates/candidatesCtrl.js
+++ components/candidates/candidatesCtrl.js
@@ -1,9 +1,5 @@
 angular.module('openDisclosure')
     .controller('candidatesCtrl', ['$scope', 'Api', function ($scope, Api) {
         $scope.candidates = Api.getCandidates(2014);
-        $scope.lastUpdate = "12/20/2014";
-        $scope.searchParams = ["lastname","firstname", "totalSpent", "total spent (%)", "campaign"];
-        $scope.isCollapsed = true;
-   }]);
-
+    }]);
 
