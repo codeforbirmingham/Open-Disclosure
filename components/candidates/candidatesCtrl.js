d69c63057abbc8e9c06f3cc362caa9ae81197b89
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-06-06 11:54:23 -0500

    Add sort button toggles.

--- components/candidates/candidatesCtrl.js
+++ components/candidates/candidatesCtrl.js
@@ -1,5 +1,11 @@
 angular.module('openDisclosure')
     .controller('candidatesCtrl', ['$scope', 'Api', function ($scope, Api) {
+        $scope.sort = {
+            name: false,
+            office: false,
+            contribution: false,
+            expenditure: false
+        };
         Api.getCandidates().then(function (candidates) {
             $scope.candidates = candidates;
         });
