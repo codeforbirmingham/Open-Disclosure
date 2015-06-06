2e328bb0e67500acc8bbbdbbc05e91e81d204bd8
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-06-06 13:35:06 -0500

    Add actual sorting.

--- components/candidates/candidatesCtrl.js
+++ components/candidates/candidatesCtrl.js
@@ -1,13 +1,21 @@
 angular.module('openDisclosure')
     .controller('candidatesCtrl', ['$scope', 'Api', function ($scope, Api) {
-        $scope.sort = {
-            name: false,
-            office: false,
-            contribution: false,
-            expenditure: false
+        $scope.sortField = 'contribution';
+        $scope.sortOrderDescending = true;
+        $scope.setSortField = function (sortField) {
+            if ($scope.sortField == sortField) {
+                // Invert order.
+                $scope.sortOrderDescending = !$scope.sortOrderDescending;
+            } else {
+                // Change sort field.
+                $scope.sortField = sortField;
+                $scope.sortOrderDescending = false;
+            }
+        };
+        $scope.getOrderBy = function () {
+            return $scope.sortOrderDescending ? '-' + $scope.sortField : $scope.sortField;
         };
         Api.getCandidates().then(function (candidates) {
             $scope.candidates = candidates;
         });
     }]);
-
