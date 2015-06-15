a7fa99351bf198117759abe652f826f41b180558
Tarif Haque <tarifhaque@gmail.com>
2015-06-15 15:08:45 -0500

    download script is now able to retrieve transactions, transactees, parties, and districts

--- data-exploration/downloadData.R
+++ data-exploration/downloadData.R
@@ -1,3 +1,26 @@
+#####################################################################
+# Use this script to download the latest aggregated Alabama campaign
+# finance transaction data from Code for Birmingham's Socrata open 
+# data portal!
+# 
+# Our data portal may be found here:
+# https://brigades.opendatanetwork.com/catalog?Brigade_Group=Code%20for%20Birmingham
+#
+# How to use this script:
+# 1. Source the script into your R environment
+# 2. Run clearData() to get rid of old data
+# 3. Get the latest data from our Socrata data portal using downloadData()
+#
+# Last revised: 15 June 2015
+# Contributor(s): Tarif Haque
+#####################################################################
+
+# Warning! Deletes all data from the data directory.
+clearData <- function() {
+  do.call(file.remove, list(list.files("data/", full.names=TRUE)))
+}
+
+# Download the latest Alabama campaign finance data from Socrata!
 downloadData <- function() {
   
   # create data directory if it does not exist
@@ -5,15 +28,44 @@ downloadData <- function() {
     dir.create("data")
   }
   
+  dateDownloaded <- date()
+  
   # download the latest transactions data if it does not exist
   if (!file.exists("data/transactions.csv")) {
     fileURL <- "https://brigades.opendatanetwork.com/api/views/vcap-yyfq/rows.csv?accessType=DOWNLOAD"
     download.file(fileURL, destfile = './data/transactions.csv', method = 'curl')
-    dateDownloaded <- date()
+    
+    # load the data
+    transactions <- read.csv("./data/transactions.csv", header = TRUE)
   }
   
-  # load the data
-  transactions <- read.csv("./data/transactions.csv", header = TRUE)
+  # download the latest parties data if it does not exist
+  if (!file.exists("data/parties.csv")) {
+    fileURL <- "https://brigades.opendatanetwork.com/api/views/ucem-puhh/rows.csv?accessType=DOWNLOAD"
+    download.file(fileURL, destfile = './data/parties.csv', method = 'curl')
+    
+    # load the data
+    parties <- read.csv("./data/parties.csv", header = TRUE)
+  }
   
-  data.frame(transactions)
+  # download the latest districts data if it does not exist
+  if (!file.exists("data/districts.csv")) {
+    fileURL <- "https://brigades.opendatanetwork.com/api/views/p8kt-epji/rows.csv?accessType=DOWNLOAD"
+    download.file(fileURL, destfile = './data/districts.csv', method = 'curl')
+    
+    # load the data
+    districts <- read.csv("./data/districts.csv", header = TRUE)
+  }
+  
+  # download the latest transactees data if it does not exist
+  if (!file.exists("data/transactees.csv")) {
+    fileURL <- "https://brigades.opendatanetwork.com/api/views/9xmj-xdkh/rows.csv?accessType=DOWNLOAD"
+    download.file(fileURL, destfile = './data/transactees.csv', method = 'curl')
+    
+    # load the data
+    transactees <- read.csv("./data/transactees.csv", header = TRUE)
+  }
+  
+  list(date = dateDownloaded, transactions = transactions, parties = parties, 
+       districts = districts, transactees = transactees)
 }
