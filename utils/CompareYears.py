ae1f8e6d35ad5d2f7e736d3737d6a6c1f39b474b
Matthew Leeds <mwl458@gmail.com>
2015-07-09 08:28:37 -0500

    Clean up CompareYears.py and don't make different records have the same id.

--- utils/CompareYears.py
+++ utils/CompareYears.py
@@ -5,28 +5,28 @@ __author__ = 'Matthew Leeds <mwl458@gmail.com>'
 import sys
 import json
 from configparser import ConfigParser
+from math import floor
 
 class CompareYears():
     """
     Examine Transactee and Party data that may span multiple years.
-    If both the name and address of a contributor match, change the more recent 
-    dataset(s) to make the unique id's match. If the name, office, and district 
-    of a candidate match, or if their openstates_id or influenceexplorer_id 
-    values match, make the unique id values match. For a PAC, a matching name 
-    is sufficient to assume equality. This means we can identify 
-    PACs/Candidates/Contributors/etc. across years. The modified transactee and 
-    party data is written back out to the disk.
+    If both the name and address of a contributor match, add the unique id's 
+    of each to the other under 'equivalent_ids'. If the name, office, and district 
+    of a candidate match, do the same. For a PAC, a matching name is sufficient to 
+    assume equality. This means we can identify PACs/Candidates/Contributors/etc. 
+    across years. The modified transactee and party data is written back out to the disk.
     """
 
     def __init__(self, config_file):
         """Read the config, initialize constants and variables for parties/transactees"""
-        self.config = configparser.ConfigParser()
+        self.config = ConfigParser()
         self.config.read(config_file)
         self.data_dir = self.config.get('COMPARE_YEARS', 'DATA_DIR')
         self.all_transactees = []
         self.transactees_set_lengths = {} # maps years to numbers of records
         self.all_parties = []
         self.parties_set_lengths = {} # maps years to numbers of records
+        # define which fields must match for each record type to be considered equivalent
         self.field_comparisons = {'Transactee': ['name', 'address'],
                                   'Candidate': ['name', 'office', 'district'],
                                   'PAC': ['name']}
@@ -39,8 +39,8 @@ class CompareYears():
         return data
     
     def load_data(self):
-        self.all_transactees = _load_file(self.config.get('GENERATE_TRANSACTEES', 'OUTFILE'))
-        self.all_parties = _load_file(self.config.get('GENERATE_PARTIES', 'OUTFILE'))
+        self.all_transactees = self._load_file(self.config.get('GENERATE_TRANSACTEES', 'OUTFILE'))
+        self.all_parties = self._load_file(self.config.get('GENERATE_PARTIES', 'OUTFILE'))
     
     def _sort_by_year(self, records):
         """
@@ -64,8 +64,8 @@ class CompareYears():
 
     def sort_by_year(self):
         """Make calls to _sort_by_year for transactees and parties"""
-        (self.all_transactees, self.transactees_set_lengths) = _sort_by_year(self.all_transactees)
-        (self.all_parties, self.parties_set_lengths) = _sort_by_year(self.all_parties)
+        (self.all_transactees, self.transactees_set_lengths) = self._sort_by_year(self.all_transactees)
+        (self.all_parties, self.parties_set_lengths) = self._sort_by_year(self.all_parties)
          
     def _compare_years(self, records, setLengths):
         """
@@ -79,9 +79,12 @@ class CompareYears():
         numModified = 0
         lastIndex = -1 # end of previous year's dataset
         for year in years[:-1]:
+            print('year='+year)
             yearStart = lastIndex + 1
             yearEnd = yearStart + setLengths[year] - 1
             for i in range(yearStart, yearEnd + 1):
+                #percentProgress = ((i - yearStart) / (yearEnd - yearStart)) * 100
+                #if percentProgress == floor(percentProgress): print(str(int(percentProgress)) + '% complete')
                 originalRecord = records[i]
                 if 'transactee_type' in originalRecord: originalRecordType = 'Transactee'
                 if 'type' in originalRecord: originalRecordType = originalRecord['type']
@@ -90,25 +93,26 @@ class CompareYears():
                     if 'transactee_type' in record: recordType = 'Transactee'
                     if 'type' in record: recordType = record['type']
                     if recordType != originalRecordType: continue
-                    '''
-                    print('recordType='+recordType)
-                    print('originalRecord='+str(originalRecord))
-                    print('record='+str(record))
-                    print('fieldComparisons='+str(fieldComparisons[recordType]))
-                    '''
                     compare = [record[field] == originalRecord[field] for field in self.field_comparisons[recordType]]
                     same = all(compare)
                     if same:
-                        #TODO should we merge them into one instead of copying the ID?
-                        record['id'] = originalRecord['id']
+                        # Add the id of each to the equivalent_ids list of the other (if it's not already there)
+                        if 'equivalent_ids' in originalRecord and record['id'] not in originalRecord['equivalent_ids']:
+                            originalRecord['equivalent_ids'].append(record['id'])
+                        elif 'equivalent_ids' not in originalRecord:
+                            originalRecord['equivalent_ids'] = [record['id']]
+                        if 'equivalent_ids' in record and originalRecord['id'] not in record['equivalent_ids']:
+                            record['equivalent_ids'].append(originalRecord['id'])
+                        elif 'equivalent_ids' not in record:
+                            record['equivalent_ids'] = [originalRecord['id']]
                         numModified += 1
             lastIndex = yearEnd
         return (records, numModified)
         
     def compare_years(self):
         """Make calls to _compare_years for transactees and parties"""
-        (self.all_transactees, transacteesModified) = _compare_years(self.all_transactees)
-        (self.all_parties, partiesModified) = _compare_years(self.all_parties)
+        (self.all_transactees, transacteesModified) = self._compare_years(self.all_transactees, self.transactees_set_lengths)
+        (self.all_parties, partiesModified) = self._compare_years(self.all_parties, self.parties_set_lengths)
         print('>> Modified ' + str(transacteesModified) + ' transactee records and ' + 
                                str(partiesModified) + ' party records')
 
@@ -122,8 +126,8 @@ class CompareYears():
         print('>> Wrote ' + str(len(records)) + ' records to ' + filename)
 
     def write_data(self):
-        _write_file(self.all_transactees, self.config.get('GENERATE_TRANSACTEES', 'OUTFILE'))
-        _write_file(self.all_parties, self.config.get('GENERATE_PARTIES', 'OUTFILE'))
+        self._write_file(self.all_transactees, self.config.get('GENERATE_TRANSACTEES', 'OUTFILE'))
+        self._write_file(self.all_parties, self.config.get('GENERATE_PARTIES', 'OUTFILE'))
 
 def main():
     compareYears = CompareYears('config.ini')
