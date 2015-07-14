8e9f5558a2bf14eb846765e81981257de6e4079e
Tait <Taitwayland@gmail.com>
2015-07-13 23:53:11 -0500

    candidate stuff

--- components/candidates/candidatesCtrl.js
+++ components/candidates/candidatesCtrl.js
@@ -1,34 +1,39 @@
 angular.module('openDisclosure')
-    .controller('candidatesCtrl', ['$scope', 'Api', function ($scope, Api) {
-        $scope.sortFields = [{
-            name: 'name',
-            label: 'Name'
-        }, {
-            name: 'office',
-            label: 'Office'
-        }, {
-            name: 'contribution',
-            label: 'Total Raised'
-        }, {
-            name: 'expenditure',
-            label: 'Total Spent'
-        }];
-        $scope.curSortField = 'contribution';
-        $scope.curSortOrderDesc = true;
-        $scope.setSortField = function (sortField) {
-            if ($scope.curSortField == sortField) {
-                // Invert order.
-                $scope.curSortOrderDesc = !$scope.curSortOrderDesc;
-            } else {
-                // Change sort field.
-                $scope.curSortField = sortField;
-                $scope.curSortOrderDesc = false;
-            }
+  .controller('candidatesCtrl', ['$scope', 'Api', function($scope, Api) {
+    $scope.sortFields = [{
+      name: 'name',
+      label: 'Name'
+    }, {
+      name: 'office',
+      label: 'Office'
+    }, {
+      name: 'contribution',
+      label: 'Total Raised'
+    }, {
+      name: 'expenditure',
+      label: 'Total Spent'
+    }];
+    $scope.curSortField = 'contribution';
+    $scope.curSortOrderDesc = true;
+    $scope.setSortField = function(sortField) {
+      if ($scope.curSortField == sortField) {
+        // Invert order.
+        $scope.curSortOrderDesc = !$scope.curSortOrderDesc;
+      } else {
+        // Change sort field.
+        $scope.curSortField = sortField;
+        $scope.curSortOrderDesc = false;
+      }
+    };
+    $scope.getOrderBy = function() {
+      return $scope.curSortOrderDesc ? '-' + $scope.curSortField : $scope.curSortField;
+    };
+    Api.getCandidates().then(function(candidates) {
+      $scope.candidates = candidates;
+    });
+    $scope.spent = function (contribution, expenditure) {
+        return {
+          width: (100 * (expenditure / (contribution + expenditure))) + "%"
         };
-        $scope.getOrderBy = function () {
-            return $scope.curSortOrderDesc ? '-' + $scope.curSortField : $scope.curSortField;
-        };
-        Api.getCandidates().then(function (candidates) {
-            $scope.candidates = candidates;
-        });
-    }]);
+    };
+  }]);
