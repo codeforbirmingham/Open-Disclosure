4acf9499c280276ab96027f13d6b744099f07b23
Tait <Taitwayland@gmail.com>
2015-10-29 18:57:24 -0500

    accidentally committed shitty datavisctrl

--- components/datavis/datavisCtrl.js
+++ components/datavis/datavisCtrl.js
@@ -1,82 +1,33 @@
 angular.module('openDisclosure')
   .controller('datavisCtrl', ['$scope', 'Api', function($scope, Api) {
 
-    $scope.sortFields = [{
-      name: 'name',
-      label: 'Name'
-    }, {
-      name: 'office',
-      label: 'Office'
-    }, {
-      name: 'contribution',
-      label: 'Total Raised'
-    }, {
-      name: 'expenditure',
-      label: 'Total Spent'
-    }];
-    $scope.curSortField = 'contribution';
-    $scope.curSortOrderDesc = true;
-    $scope.setSortField = function(sortField) {
-      if ($scope.curSortField === sortField) {
-        // Invert order.
-        $scope.curSortOrderDesc = !$scope.curSortOrderDesc;
-      } else {
-        // Change sort field.
-        $scope.curSortField = sortField;
-        $scope.curSortOrderDesc = false;
-      }
-    };
-    $scope.getOrderBy = function() {
-      return $scope.curSortOrderDesc ? '-' + $scope.curSortField : $scope.curSortField;
-    };
+    var initial = [23, 22, 21, 21, 23, 50, 102];
+    $scope.circles = [];
 
-    function getSpentPercentage() {
-      console.log('works');
-      return "got percentage";
+
+    function initializeCircle() {
+        var circle = d3.select("svg").selectAll("circle")
+        .data(initial)
+        .enter()
+        .append("svg")
+        .attr("cx", 230)
+        .text(function(d){ return d; })
+        .attr("cy", 230)
+        .attr("r", function(d) {
+          return d;
+        })
+        .style("fill", "red")
+        .on("mouseover", function() {
+          d3.select(this)
+          .style("fill", "#67a1dd");
+        })
+        .on("mouseout", function() {
+          d3.select(this)
+          .style("fill", "green");
+        });
     }
 
-
-
-    Api.getCandidates().then(function(candidates) {
-      $scope.candidates = candidates;
-
-      var svg = d3.select(document.getElementById("datavis"))
-      .append("svg");
-      .style('width', '100%');
-      // Browser onresize event
-      window.onresize = function() {
-        $scope.$apply();
-      };
-
-      // hard-code data
-      $scope.mockdata = [{
-        name: "Greg",
-        score: 98
-      }, {
-        name: "Ari",
-        score: 96
-      }, {
-        name: 'Q',
-        score: 75
-      }, {
-        name: "Loser",
-        score: 48
-      }];
-
-      // Watch for resize event
-            $scope.$watch(function() {
-              return angular.element($window)[0].innerWidth;
-            }, function() {
-              $scope.render($scope.mockdata);
-            });
-
-            scope.render = function(data) {
-              // our custom d3 code
-            };
-
-
-    });
-
+    initializeCircle();
 
 
   }]);
